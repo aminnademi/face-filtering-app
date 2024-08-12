a18ebe6cb6ab9272c6d404387dcc9dff3cb5cca1
aminnademi <mgamer7557@gmail.com>
2024-08-12 20:57:33 +0330

    create the FaceDetector class that extract the face features and recognize that an image contain frontface or not with them

---
+++ utils/face_detector.py
@@ -0,0 +1,27 @@
+from retinaface import RetinaFace
+
+class FaceDetector:
+    def __init__(self):
+        pass
+
+    def detect_faces(self, image_path: str):
+        # Detect faces using RetinaFace
+        faces = RetinaFace.detect_faces(image_path)
+        return faces
+
+    def face_features(self, faces):
+        features = []
+        for face in faces.values():
+            landmarks = face['landmarks']
+            left_eye = landmarks['left_eye']
+            right_eye = landmarks['right_eye']
+            nose = landmarks['nose']
+            features.append((left_eye, right_eye, nose))
+        return features
+
+    def is_frontface(self, features):
+        for i in features:
+            left_eye, right_eye, nose = i
+            if abs(left_eye[0] - right_eye[0]) < 10 and abs((left_eye[1] + right_eye[1]) / 2 - nose[1]) < 10:
+                return True
+        return False
